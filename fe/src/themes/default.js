642144bd2b044167fafcf9c8eed1669fb3b03f3f
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2025-01-06 23:13:27 -0300

    style: default styles refactored

--- fe/src/themes/default.js
+++ fe/src/themes/default.js
@@ -4,8 +4,8 @@ export default {
         primary: {
             lighter: "#e0e3ff",
             light: "#6674f4",
-            main: "#5061fc",
             dark: "#3346f0",
+            main: "#5061fc",
         },
     },
 };
