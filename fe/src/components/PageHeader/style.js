15be82d9bde7cd63388b033793e0c19c211ede6e
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2025-01-11 09:57:28 -0300

    styles: fixed display flex styling to inline flex on back link

--- fe/src/components/PageHeader/style.js
+++ fe/src/components/PageHeader/style.js
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 
 export const Container = styled.header`
+    margin-bottom: 24px;
     a {
         color: ${({ theme }) => theme.colors.primary.main};
         text-decoration: none;
         font-weight: bold;
-        display: flex;
+        display: inline-flex;
         align-items: center;
     }
 `;
