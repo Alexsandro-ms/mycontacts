8a79e03134c08f65d16f83f43da92b7502d74c0a
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2025-01-10 22:17:40 -0300

    refactor: refactored the home page and applied default margin bottom to the logo

--- fe/src/components/Header/index.js
+++ fe/src/components/Header/index.js
@@ -1,4 +1,5 @@
-import { Container, ImageHeader, InputSearchContainer } from "./style";
+import React from "react";
+import { Container, ImageHeader } from "./style";
 
 import LogoSvg from "../../assets/images/logo.svg";
 
@@ -6,9 +7,6 @@ export default function Header() {
     return (
         <Container>
             <ImageHeader src={LogoSvg} alt="MyContacts" />
-            <InputSearchContainer>
-                <input type="text" placeholder="Pesquise pelo nome..." />
-            </InputSearchContainer>
         </Container>
     );
 }
--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -1,5 +1,50 @@
 import React from "react";
+import { ArrowUp, NotePencil, TrashSimple } from "@phosphor-icons/react";
+import {
+    Card,
+    Container,
+    Header,
+    ListContainer,
+    InputSearchContainer,
+} from "./styles";
 
-export default function HomePage() {
-    return "Home Page";
+export default function Home() {
+    return (
+        <Container>
+            <InputSearchContainer>
+                <input type="text" placeholder="Pesquise pelo nome..." />
+            </InputSearchContainer>
+            <Header>
+                <strong>3 contatos</strong>
+                <a href="/">Novo Contato</a>
+            </Header>
+            <ListContainer>
+                <header>
+                    <button type="button">
+                        <span>Nome</span>
+                        <ArrowUp size={16} />
+                    </button>
+                </header>
+                <Card>
+                    <div className="info">
+                        <div className="contact-name">
+                            <strong>John Doe</strong>
+                            <small>Instagram</small>
+                        </div>
+                        <span>johndoe@example.com</span>
+                        <span>(00) 9 9999-9999</span>
+                    </div>
+
+                    <div className="actions">
+                        <a href="/">
+                            <NotePencil size={24} color="#5061fc" />
+                        </a>
+                        <button type="button">
+                            <TrashSimple size={24} color="red" />
+                        </button>
+                    </div>
+                </Card>
+            </ListContainer>
+        </Container>
+    );
 }
