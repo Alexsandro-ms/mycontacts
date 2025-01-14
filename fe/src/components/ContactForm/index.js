cc89eddefe6e80d77375c16e8573e6a11ea93a3f
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2025-01-13 21:49:47 -0300

    refactor: contactform refactored

--- fe/src/components/ContactForm/index.js
+++ fe/src/components/ContactForm/index.js
@@ -1,8 +1,9 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-import { Form, ButtonContainer } from "./styles";
 import FormGroup from "../FormGroup";
+
+import { Form, ButtonContainer } from "./styles";
 import { Input } from "../Input";
 import { Select } from "../Select";
 import { Button } from "../Button";
