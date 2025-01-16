14a57899f1b830a6514bb07177f197ad318e30c9
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2025-01-15 21:54:47 -0300

    feat: validated the name and email fields
    - Added custom hook 'useError' to handle errors;
    - Created states to store form information and added temporary submit function for testing.

--- fe/src/components/ContactForm/index.js
+++ fe/src/components/ContactForm/index.js
@@ -1,28 +1,81 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-import FormGroup from "../FormGroup";
+import isEmailValid from "../../utils/isEmailValid";
+import useErrors from "../../hooks/useErrors";
 
+import FormGroup from "../FormGroup";
 import { Form, ButtonContainer } from "./styles";
 import { Input } from "../Input";
 import { Select } from "../Select";
 import { Button } from "../Button";
 
 export default function ContactForm({ buttonLabel }) {
+    const [isName, setIsName] = useState("");
+    const [isEmail, setIsEmail] = useState("");
+    const [isPhone, setIsPhone] = useState("");
+    const [isCategory, setIsCategory] = useState("");
+    const { getErrorMessageByFieldName, removeError, setError } = useErrors();
+
+    function handleNameChange(e) {
+        setIsName(e.target.value);
+        if (!e.target.value) {
+            setError({ field: "name", message: "Nome é obrigatório." });
+        } else {
+            removeError("name");
+        }
+    }
+    function handleEmailChange(e) {
+        setIsEmail(e.target.value);
+        if (e.target.value && !isEmailValid(e.target.value)) {
+            setError({ field: "email", message: "E-mail inválido." });
+        } else {
+            removeError("email");
+        }
+    }
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log({
+            name: isName,
+            email: isEmail,
+            phone: isPhone,
+            category: isCategory,
+        });
+    }
+
     return (
-        <Form>
-            <FormGroup>
-                <Input placeholder="Nome" />
+        <Form onSubmit={handleSubmit}>
+            <FormGroup error={getErrorMessageByFieldName("name")}>
+                <Input
+                    error={getErrorMessageByFieldName("name")}
+                    placeholder="Nome"
+                    value={isName}
+                    onChange={handleNameChange}
+                />
+            </FormGroup>
+            <FormGroup error={getErrorMessageByFieldName("email")}>
+                <Input
+                    error={getErrorMessageByFieldName("email")}
+                    placeholder="E-mail"
+                    value={isEmail}
+                    onChange={handleEmailChange}
+                />
             </FormGroup>
             <FormGroup>
-                <Input placeholder="E-mail" />
+                <Input
+                    placeholder="Telefone"
+                    value={isPhone}
+                    onChange={(e) => setIsPhone(e.target.value)}
+                />
             </FormGroup>
             <FormGroup>
-                <Input placeholder="Telefone" />
-            </FormGroup>
-            <FormGroup>
-                <Select>
-                    <option value="instagram">Instagram</option>
+                <Select
+                    value={isCategory}
+                    onChange={(e) => setIsCategory(e.target.value)}
+                >
+                    <option value={""}>Categoria</option>
+                    <option value={"instagram"}>Instagram</option>
+                    <option value={"discord"}>Discord</option>
                 </Select>
             </FormGroup>
             <ButtonContainer>
