724264e24f3ecababd9a6a9ba7db19e92870f056
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2025-02-01 20:50:37 -0300

    feat: setting important cors settings

--- api/src/app/middlewares/cors.js
+++ api/src/app/middlewares/cors.js
@@ -1,4 +1,7 @@
 module.exports = (request, response, next) => {
-    response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+    response.setHeader("Access-Control-Allow-Origin", "http://localhost:3000"); // Define a origem
+    response.setHeader("Access-Control-Allow-Methods", "*"); // Define os métodos
+    response.setHeader("Access-Control-Allow-Headers", "*"); // Define os cabeçalhos
+    response.setHeader("Access-Control-Control-Max-Age", "10"); // Define o tempo de vida da requisição armazenando em cache
     next();
 };
