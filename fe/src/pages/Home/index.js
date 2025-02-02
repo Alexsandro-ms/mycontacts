3437af076b1a683a79e25c535b63325a6ff9b6fb
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2025-02-01 21:52:12 -0300

    feat: Created the functionality to change the order of the contact list

--- fe/src/pages/Home/index.js
+++ fe/src/pages/Home/index.js
@@ -1,19 +1,24 @@
 import React, { useEffect, useState } from "react";
-import { ArrowUp, NotePencil, TrashSimple } from "@phosphor-icons/react";
+import {
+    ArrowDown,
+    ArrowUp,
+    NotePencil,
+    TrashSimple,
+} from "@phosphor-icons/react";
 import {
     Card,
     Container,
     Header,
-    ListContainer,
+    ListHeader,
     InputSearchContainer,
 } from "./styles";
 import { Link } from "react-router-dom";
 
 export default function Home() {
     const [contacts, setContacts] = useState([]);
-
+    const [orderBy, setOrderBy] = useState("asc");
     useEffect(() => {
-        fetch("http://localhost:3001/contacts")
+        fetch(`http://localhost:3001/contacts?orderBy=${orderBy}`)
             .then(async (response) => {
                 const json = await response.json();
                 setContacts(json);
@@ -21,7 +26,11 @@ export default function Home() {
             .catch((error) => {
                 console.error("Erro", error);
             });
-    }, []);
+    }, [orderBy]);
+
+    function handleToggleOrderBy() {
+        setOrderBy((prevState) => (prevState === "asc" ? "desc" : "asc"));
+    }
 
     return (
         <Container>
@@ -35,37 +44,40 @@ export default function Home() {
                 </strong>
                 <Link to="/new">Novo Contato</Link>
             </Header>
-            <ListContainer>
-                <header>
-                    <button type="button">
-                        <span>Nome</span>
-                        <ArrowUp size={16} />
-                    </button>
-                </header>
-                {contacts.map((contact) => (
-                    <Card key={contact.id}>
-                        <div className="info">
-                            <div className="contact-name">
-                                <strong>{contact.name}</strong>
-                                {contact.category_name && (
-                                    <small>{contact.category_name}</small>
-                                )}
-                            </div>
-                            <span>{contact.email}</span>
-                            <span>{contact.phone}</span>
-                        </div>
+            <ListHeader>
+                <button type="button" onClick={handleToggleOrderBy}>
+                    <span>Nome</span>
+                    {orderBy === "asc" ? (
+                        <ArrowUp size={16} color="#5061fc" />
+                    ) : (
+                        <ArrowDown size={16} color="#5061fc" />
+                    )}
+                </button>
+            </ListHeader>
 
-                        <div className="actions">
-                            <Link to={`/edit/${contact.id}`}>
-                                <NotePencil size={24} color="#5061fc" />
-                            </Link>
-                            <button type="button">
-                                <TrashSimple size={24} color="red" />
-                            </button>
+            {contacts.map((contact) => (
+                <Card key={contact.id}>
+                    <div className="info">
+                        <div className="contact-name">
+                            <strong>{contact.name}</strong>
+                            {contact.category_name && (
+                                <small>{contact.category_name}</small>
+                            )}
                         </div>
-                    </Card>
-                ))}
-            </ListContainer>
+                        <span>{contact.email}</span>
+                        <span>{contact.phone}</span>
+                    </div>
+
+                    <div className="actions">
+                        <Link to={`/edit/${contact.id}`}>
+                            <NotePencil size={24} color="#5061fc" />
+                        </Link>
+                        <button type="button">
+                            <TrashSimple size={24} color="red" />
+                        </button>
+                    </div>
+                </Card>
+            ))}
         </Container>
     );
 }
