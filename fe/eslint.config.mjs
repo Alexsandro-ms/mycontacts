a79c853a22d4b80b4e80b6481c8589bb75fc894b
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2025-02-01 23:12:31 -0300

    chore: added eslint-plugin-react-hooks dependecy

--- fe/eslint.config.mjs
+++ fe/eslint.config.mjs
@@ -2,11 +2,20 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import pluginReact from "eslint-plugin-react";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,jsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  pluginReact.configs.flat.recommended,
+    {
+        files: ["**/*.{js,mjs,cjs,jsx}"],
+        languageOptions: { globals: globals.browser },
+        extends: ["plugin:react/recommended"],
+        plugins: ["react"],
+        rules: {
+            "react/react-in-jsx-scope": "off",
+            "import/prefer-default-export": "off",
+            "react-hooks/rules-of-hooks": "error",
+            "react-hooks/exhaustive-deps": "warn",
+        },
+    },
+    pluginJs.configs.recommended,
+    pluginReact.configs.flat.recommended,
 ];
