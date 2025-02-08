3e43424fa99cee3649289851c67e9a78c37e4164
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2025-02-08 12:12:07 -0300

    feat: create CategoriesService to manage category data
    - Implemented the 'CategoriesService' class to abstract category-related API calls.
    - Added the 'HttpClient' dependency to simplify HTTP request operations.
    - Added the 'listCategories' method to fetch the list of categories from th
    - Implemented the 'CategoriesService' class to abstract category-related API calls.
    - Added the 'HttpClient' dependency to simplify HTTP request operations.
    - Added the 'listCategories' method to fetch the list of categories from the API

---
+++ fe/src/services/CategoriesService.js
@@ -0,0 +1,12 @@
+import HttpClient from "./utils/HttpClient";
+
+class CategoriesService {
+    constructor() {
+        this.httpClient = new HttpClient("http://localhost:3001"); // definindo uma instância do HttpClient
+    }
+    async listCategories() {
+        return this.httpClient.get("/categories"); // chamando o método get do HttpClient
+    }
+}
+
+export default new CategoriesService();
