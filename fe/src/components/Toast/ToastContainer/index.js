02ae71f476baf6912f75c114f45c34c18e315019
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2025-02-16 18:19:44 -0300

    refactor: add ToastContainer component with default messages
    - Adds the ToastContainer component, responsible for displaying toast messages. Initially, it contains predefined messages of the types default, danger and success.

--- fe/src/components/Toast/ToastContainer/index.js
+++ fe/src/components/Toast/ToastContainer/index.js
@@ -1,9 +1,22 @@
 import { Container } from "./styles";
+import ToastMessage from "../ToastMessage";
+import { useState } from "react";
 
 export default function ToastContainer() {
+    const [message] = useState([
+        { id: Math.random(), type: "default", text: "Default toast" },
+        { id: Math.random(), type: "danger", text: "Danger toast" },
+        { id: Math.random(), type: "success", text: "Success toast" },
+    ]);
     return (
         <Container>
-            <h1>Toast Container</h1>
+            {message.map((message) => (
+                <ToastMessage
+                    key={message.id}
+                    type={message.type}
+                    text={message.text}
+                />
+            ))}
         </Container>
     );
 }
