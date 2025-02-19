53fa1b56f8990f6962699514716cbde6c4cdbfd0
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2025-02-18 21:10:58 -0300

    feat: adds ToastContainer for displaying notifications
    - Integrates  to manage and display toast notifications.

--- fe/src/components/App/index.js
+++ fe/src/components/App/index.js
@@ -9,12 +9,14 @@ import { ThemeProvider } from "styled-components";
 import defaultTheme from "../../themes/default";
 import { Container } from "./styles";
 import Header from "../Header";
+import ToastContainer from "../Toast/ToastContainer";
 
 export default function App() {
     return (
         <BrowserRouter>
             <ThemeProvider theme={defaultTheme}>
                 <GlobalStyles />
+                <ToastContainer />
                 <Container>
                     <Header />
                     <Routes />
