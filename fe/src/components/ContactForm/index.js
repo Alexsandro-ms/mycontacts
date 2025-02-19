f3d34cc08c83e27f0242545e1a8d788a117c6f08
Alexsandro Martins <alexsandromartins.santos@hotmail.com>
2025-02-18 21:06:47 -0300

    refactor: adjust field name when sending data to 'categoryId'
    - Changes the  key to  when submitting form data.
    - Fixes the data submission structure to ensure the category is correctly identified.

--- fe/src/components/ContactForm/index.js
+++ fe/src/components/ContactForm/index.js
@@ -64,7 +64,7 @@ export default function ContactForm({ buttonLabel, onSubmit }) {
             name: isName,
             email: isEmail,
             phone: isPhone.replace(/\D/g, ""),
-            category: categoryId,
+            categoryId,
         });
         setIsSubmitting(false);
     }
